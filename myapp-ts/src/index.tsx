5bfc19900ea2ad9785178cdd27bb373695d5cfde
konishi.kenji <konishi.b0engineer@gmail.com>
2019-07-19 17:38:49 +0900

    備忘録を一部修正

--- myapp-ts/src/index.tsx
+++ myapp-ts/src/index.tsx
@@ -5,7 +5,8 @@ import * as ReactDOM from "react-dom";
 // import registerServiceWorker from './registerServiceWorker';
 import App from "./component/App";
 // 以下2行は入れ替え不可（「Failed to compile.」が起きる）
-// (8, 1): Import sources within a group must be alphabetized.(（8, 1）：グループ内のインポート元はアルファベット順にする必要があります。)
+// (8, 1): Import sources within a group must be alphabetized.
+// (（8, 1）：グループ内のインポート元はアルファベット順にする必要があります。)
 import registerServiceWorker from "./materials/registerServiceWorker";
 import "./stypes/index.css";
 
